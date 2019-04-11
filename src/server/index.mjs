5e235ee7b40ec4ffcb098a05482ad1839108d0d4
Fi1osof <n.lanets@modxclub.ru>
2019-04-11 04:02:05 +0300

    1.5.5

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -25,9 +25,23 @@ if (!GethServer) {
   throw ("Env GethServer required");
 }
 
-const web3 = new Web3();
-web3.setProvider(new web3.providers.HttpProvider(GethServer));
 
+const web3Options = {
+  defaultAccount: '0x0',
+  defaultBlock: 'latest',
+  defaultGas: 1,
+  defaultGasPrice: 0,
+  transactionBlockTimeout: 50,
+  transactionConfirmationBlocks: 24,
+  transactionPollingTimeout: 480,
+  // transactionSigner: new CustomTransactionSigner(),
+}
+
+// const web3 = new Web3(GethServer);
+const web3 = new Web3(GethServer);
+// web3.setProvider(new web3.providers.HttpProvider(GethServer), null, web3Options);
+
+// console.log("web3", web3);
 
 startServer({
   typeDefs: 'src/schema/generated/api.graphql',
